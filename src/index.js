8faba2ad8a4462ab9a7a3d6c28f0c719debc4b2e
Pascal Birchler <pascalb@google.com>
2022-09-19 10:13:10 +0200

    Lint fix

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import {
 	unregisterBlockType,
 	getBlockVariations,
 	unregisterBlockVariation,
-	getBlockType
+	getBlockType,
 } from '@wordpress/blocks';
 
 import domReady from '@wordpress/dom-ready';
 
-domReady( () => {
-	if ( getBlockVariations && getBlockVariations( 'core/embed' ) ) {
-		unregisterBlockVariation( 'core/embed', 'wordpress' );
-	} else if ( getBlockType( 'core-embed/wordpress') ) {
-		unregisterBlockType( 'core-embed/wordpress' );
+domReady(() => {
+	if (getBlockVariations && getBlockVariations('core/embed')) {
+		unregisterBlockVariation('core/embed', 'wordpress');
+	} else if (getBlockType('core-embed/wordpress')) {
+		unregisterBlockType('core-embed/wordpress');
 	}
-} );
+});
