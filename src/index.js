05eff7a3390e371f9ee15f8c4083e7d7cc35c79c
Pascal Birchler <pascalb@google.com>
2022-09-19 11:10:29 +0200

    Lint fixes

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import {
 
 import domReady from '@wordpress/dom-ready';
 
-domReady(() => {
-	if (getBlockVariations && getBlockVariations('core/embed')) {
-		unregisterBlockVariation('core/embed', 'wordpress');
-	} else if (getBlockType('core-embed/wordpress')) {
-		unregisterBlockType('core-embed/wordpress');
+domReady( () => {
+	if ( getBlockVariations && getBlockVariations( 'core/embed' ) ) {
+		unregisterBlockVariation( 'core/embed', 'wordpress' );
+	} else if ( getBlockType( 'core-embed/wordpress' ) ) {
+		unregisterBlockType( 'core-embed/wordpress' );
 	}
-});
+} );
